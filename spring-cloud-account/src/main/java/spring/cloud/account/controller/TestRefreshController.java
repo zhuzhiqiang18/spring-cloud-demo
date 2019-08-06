6f604d0e70b66a222fbfc0119779e14978ece9da
朱志强 <zhuzhiqiang@19lou.com>
2019-08-06 16:41:18 +0800

    测试@RefreshScope

--- spring-cloud-account/src/main/java/spring/cloud/account/controller/TestRefreshController.java
+++ spring-cloud-account/src/main/java/spring/cloud/account/controller/TestRefreshController.java
@@ -1,7 +1,21 @@
 package spring.cloud.account.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 测试配置刷新
  */
+@RestController
+@RefreshScope
 public class TestRefreshController {
+    @Value("${refresh.test}")
+    public String test;
+
+    @RequestMapping("/test")
+    public String test(){
+        return test;
+    }
 }
